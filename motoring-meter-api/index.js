8531faab57f27018fcb1d27332434338d10438db
Sami Oksanen <sami.oksanen95@gmail.com>
2021-05-31 23:35:33 +0300

    Fuel consumption comparison

--- motoring-meter-api/index.js
+++ motoring-meter-api/index.js
@@ -3,19 +3,31 @@ const cors = require('cors');
 const app = express();
 
 const cars = [
-    {"id": "1", "desc": "Auto A: 3l / 100km", "baseConsumption": 3, "consumptionExponent": 1.009},
-    {"id": "2", "desc": "Auto B: 3.5l / 100km", "baseConsumption": 3.5, "consumptionExponent": 1.009},
-    {"id": "3", "desc": "Auto C: 4l / 100km", "baseConsumption": 4, "consumptionExponent": 1.009}
+    { "id": "1", "desc": "Auto A: 3l / 100km", "baseConsumption": 3, "consumptionExponent": 1.009 },
+    { "id": "2", "desc": "Auto B: 3.5l / 100km", "baseConsumption": 3.5, "consumptionExponent": 1.009 },
+    { "id": "3", "desc": "Auto C: 4l / 100km", "baseConsumption": 4, "consumptionExponent": 1.009 }
 ];
 
 function countFuelConsumption(carId, distance, speed) {
     const car = cars.find(x => x.id === carId);
-    return (Math.round(car.baseConsumption / 100 * (car.consumptionExponent ** speed) * distance * 100) / 100) + 'l / 100km';
+    const consumption = Math.round(car.baseConsumption / 100 * (car.consumptionExponent ** speed) * distance * 100) / 100;
+    return { "value": consumption, "desc": getConsumptionDesc(consumption) };
+}
+
+function getConsumptionDesc(consumption) {
+    let consumptionNum = Math.round(Number(consumption.toString().replace('+', '').replace('-', '')) * 100) / 100;
+    let consumptionDesc = '';
+    if (consumption.toString().startsWith('+-')) {
+        consumptionDesc = ' less';
+    } else if (consumption.toString().startsWith('+')) {
+        consumptionDesc = ' more';
+    }
+    return consumptionNum.toString() + ' litres per 100km' + consumptionDesc;
 }
 
 function countDuration(distance, speed) {
     let duration = distance / speed;
-    return {"value": duration, "desc": getDurationDesc(duration.toString())};
+    return { "value": duration, "desc": getDurationDesc(duration.toString()) };
 }
 
 function getDurationDesc(duration) {
@@ -33,7 +45,11 @@ function getDurationDesc(duration) {
 }
 
 app.get('/consumption', cors(), (req, res) => {
-    res.send(countFuelConsumption(req.query.carid, req.query.distance, req.query.speed).toString());
+    res.send(countFuelConsumption(req.query.carid, req.query.distance, req.query.speed));
+});
+
+app.get('/consumptiondesc', cors(), (req, res) => {
+    res.send(getConsumptionDesc(req.query.consumption).toString());
 });
 
 app.get('/duration', cors(), (req, res) => {
--- motoring-meter-ui/src/App.js
+++ motoring-meter-ui/src/App.js
@@ -6,7 +6,20 @@ function App() {
 
     const [car, setCar] = useState([{ id: '', desc: '', baseConsumption: 0, consumptionExponent: 0 }]);
 
-    const [ride, setRide] = useState({ carid: '', distance: '', speed1: '', consumption1: '', duration1: { value: '', desc: '' }, duration1vsDuration2: '', speed2: '', consumption2: '', duration2: { value: '', desc: '' }, duration2vsDuration1: '' });
+    const [ride, setRide] = useState({ 
+        carid: '', 
+        distance: '', 
+        speed1: '', 
+        consumption1: { value: '', desc: '' }, 
+        consumption1vsConsumption2: '', 
+        duration1: { value: '', desc: '' }, 
+        duration1vsDuration2: '', 
+        speed2: '', 
+        consumption2: { value: '', desc: '' },
+        consumption2vsConsumption1: '', 
+        duration2: { value: '', desc: '' }, 
+        duration2vsDuration1: '' 
+    });
 
     function getCars() {
         fetch(api + '/cars')
@@ -50,9 +63,20 @@ function App() {
     function updateConsumption(x) {
         if (ride.carid && ride.distance && ride['speed' + x]) {
             fetch(api + '/consumption?carid=' + ride.carid + '&distance=' + ride.distance + '&speed=' + ride['speed' + x])
+                .then(res => res.json())
+                .then(json => {
+                    setRide(prevRide => ({ ...prevRide, ['consumption' + x]: json }));
+                });
+        }
+    }
+
+    function updateConsumptionVersus(x, y) {
+        if (ride.distance && ride['speed' + x] && ride['speed' + y]) {
+            const durationVersus = '+' + (Number(ride['consumption' + x].value) - Number(ride['consumption' + y].value)).toString();
+            fetch(api + '/consumptiondesc?consumption=' + encodeURIComponent(durationVersus))
                 .then(res => res.text())
                 .then(text => {
-                    setRide(prevRide => ({ ...prevRide, ['consumption' + x]: text }));
+                    setRide(prevRide => ({ ...prevRide, ['consumption' + x + 'vsConsumption' + y]: ' which is ' + text }));
                 });
         }
     }
@@ -77,6 +101,11 @@ function App() {
         updateConsumption('2');
     }, [ride.carid]);
 
+    useEffect(() => {
+        updateConsumptionVersus('1', '2');
+        updateConsumptionVersus('2', '1');
+    }, [ride.consumption1, ride.consumption2]);
+
     return (
         <div>
             <header>
@@ -93,10 +122,10 @@ function App() {
                 <p>Distance: <input key="distance" name="distance" onChange={e => updateRide(e)} /></p>
                 <p>Speed 1: <input key="speed1" name="speed1" onChange={e => updateRide(e)} /></p>
                 <p key="duration1">Duration 1: {ride.duration1.desc} {ride.duration1vsDuration2}</p>
-                <p key="consumption1">Consumption 1: {ride.consumption1}</p>
+                <p key="consumption1">Consumption 1: {ride.consumption1.desc} {ride.consumption1vsConsumption2}</p>
                 <p>Speed 2: <input key="speed2" name="speed2" onChange={e => updateRide(e)} /></p>
                 <p key="duration2">Duration 2: {ride.duration2.desc} {ride.duration2vsDuration1}</p>
-                <p key="consumption2">Consumption 2: {ride.consumption2} </p>
+                <p key="consumption2">Consumption 2: {ride.consumption2.desc} {ride.consumption2vsConsumption1}</p>
             </div>
             <footer>
                 Made by Sami Oksanen
